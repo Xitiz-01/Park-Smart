76d502b5408b888880671c20037b51b3faa0ff45
Kshitij Londhe <kshitij.londhe01@gmail.com>
2026-04-12 19:24:35 +0530

    fixed api route

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/api", (req, res) => {
+  res.send("API is working");
+});
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/slots', require('./routes/slotRoutes'));
@@ -42,10 +44,6 @@ app.use('/api/vehicles', require('./routes/vehicleRoutes'));
 app.get('/', (req, res) => {
   res.json({ message: 'Parking System API is running' });
 });
-app.get("/api", (req, res) => {
-  res.send("API is working");
-});
-
 // Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
